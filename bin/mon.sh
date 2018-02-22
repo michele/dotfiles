26c9897a7c459140c0a09732fcd03c10200888ba
Michele Finotto <m@finotto.org>
2018-02-22 12:10:05 +0100

    More customizations, mainly for external devices

--- bin/mon.sh
+++ bin/mon.sh
@@ -1,6 +1,21 @@
 total=$(xrandr --current | grep '\bconnected' | wc -l)
 outputs="--output eDP-1 --primary --auto --pos 581x2376 --dpi 220 --scale 1x1"
 
+xrandr --output DP-2 --off
+
+if xrandr | grep "DP-2 connected"; then
+	outputs+=" --output DP-2 --auto --pos 0x0 --above eDP-1 --scale 1.3x1.3"
+fi
+
+xrandr $outputs
+
+if xrandr | grep "DP-2 connected"; then
+	xrandr --output DP-2 --pos 0x0
+	xrandr --output eDP-1 --pos 581x2808
+fi
+
+exit 0
+
 xrandr --output DP-2-1 --off
 
 if xrandr | grep "DP-2-1 connected"; then
--- bin/mx-scroll.sh
+++ bin/mx-scroll.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-xinput set-prop "pointer:MX Master 2S" --type=int --format=8 "Natural Scrolling Enabled" 1
+dev=`xinput list | grep 'MX Master 2S.*pointer'`
+mxreg="^.*id=([0-9]+).*$"
+if [[ $dev =~ $mxreg ]];
+then
+    did="${BASH_REMATCH[1]}"
+    devprop=`xinput list-props $did | grep 'libinput Natural Scrolling Enabled ('`
+    propreg="^.*\(([0-9]+)\).*"
+    if [[ $devprop =~ $propreg ]];
+    then
+        echo 'Found logitech mx mouse'
+        xinput set-prop $did --type=int ${BASH_REMATCH[1]} 1
+    fi
+else
+    echo 'nope'
+fi
