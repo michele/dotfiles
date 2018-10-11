c083f564bac2f2a2269f6f5996ecf1551ae4a771
Michele Finotto <m@finotto.org>
2018-10-11 11:54:41 +0200

    Various updates

--- bin/mx-scroll.sh
+++ bin/mx-scroll.sh
@@ -16,6 +16,22 @@ else
     echo 'nope'
 fi
 
+dev=`xinput list | grep 'Synaptics TM3288'`
+mxreg="^.*id=([0-9]+).*$"
+if [[ $dev =~ $mxreg ]];
+then
+    did="${BASH_REMATCH[1]}"
+    devprop=`xinput list-props $did | grep 'libinput Natural Scrolling Enabled ('`
+    propreg="^.*\(([0-9]+)\).*"
+    if [[ $devprop =~ $propreg ]];
+    then
+        echo 'Found logitech mx mouse'
+        xinput set-prop $did --type=int ${BASH_REMATCH[1]} 1
+    fi
+else
+    echo 'nope'
+fi
+
 dev=`xinput list | grep 'MX Master 2S.*pointer'`
 mxreg="^.*id=([0-9]+).*$"
 if [[ $dev =~ $mxreg ]];
